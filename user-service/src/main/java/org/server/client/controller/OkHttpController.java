703fef0ee41bcc7b8967e72a3ceef50074acaf47
軒 <lzz00826@gmail.com>
2023-11-20 19:03:48 +0800

    log4j2

--- user-service/src/main/java/org/server/client/controller/OkHttpController.java
+++ user-service/src/main/java/org/server/client/controller/OkHttpController.java
@@ -8,13 +8,19 @@ import lombok.extern.log4j.Log4j2;
 import org.server.client.obj.Order;
 import org.server.client.obj.Results;
 import org.server.client.uitl.OkHttpUtil;
+import org.server.util.LogUtils;
+import org.server.util.LogUtils.LogType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Log4j2
 @RestController
 public class OkHttpController {
 
+  Logger logger = LogManager.getLogger(getClass());
 
   @GetMapping("/test/okHttp")
   public String sendMsg() {
@@ -27,6 +33,12 @@ public class OkHttpController {
   public String sendForm() {
     String url = "http://localhost:8081/order/order/test/form";
     Map<String, String> params = new HashMap<>();
+    logger.debug("message");
+    logger.info("message");
+    logger.error("message");
+    logger.trace("message");
+    logger.warn("message");
+    LogUtils.data(LogType.Error,"testTag",params);
     params.put("id", "3442");
     params.put("userId", "1");
     params.put("price", "123");
